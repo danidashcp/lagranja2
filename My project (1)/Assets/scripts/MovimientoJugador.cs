073c162ef1bf8abab6927888dc212c77c38debb8
unknown <dannapaolavillegas@gmail.com>
2025-06-09 12:45:18 -0600

    gatos

---
+++ My project (1)/Assets/scripts/MovimientoJugador.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovimientoJugador : MonoBehaviour
+{
+	public float velocidad = 5f;
+	public Rigidbody2D rg;
+    public Vector2 entrada;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+         rg.linearVelocity = entrada * velocidad;
+        
+    }
+
+    public void Moverse(InputAction.CallbackContext contexto) {
+          entrada = contexto.ReadValue<Vector2>();
+
+    }
+}
+
+
